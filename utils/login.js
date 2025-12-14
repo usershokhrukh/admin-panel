fcd18c06326e16b04949321846e6c51bf6ca6a08
usershokhrukh <asxurovsit@gmail.com>
2025-12-14 17:42:26 +0500

    features: login 100%

--- utils/login.js
+++ utils/login.js
@@ -1,1 +1,91 @@
+const elLogin = document.querySelector(".login-form");
+const elHide = document.querySelector(".hide");
+const elShow = document.querySelector(".show");
+const elTitle = document.querySelector("title");
+const elLoader = document.querySelector(".login__loader");
+const elButtonSpan = document.querySelector(".login__button-span");
+let toast = false;
+elTitle.textContent = "Login";
+
+elHide.addEventListener("click", ()=> {
+  elHide.classList.toggle("none");
+  elShow.classList.toggle("none");
+  elLogin["password"].type = "text";
+})
+
+elShow.addEventListener("click", ()=> {
+  elHide.classList.toggle("none");
+  elShow.classList.toggle("none");
+  elLogin["password"].type = "password";
+})
+
+
+elLogin.addEventListener("submit" , (e) => {
+  e.preventDefault();
+  const elUsername = elLogin["username"].value.trim();
+  const elPassword = elLogin["password"].value.trim();
+  const api = 'https://fakestoreapi.com/auth/login';
+  const dataUser = {
+    username: elUsername,
+    password: elPassword,
+  }
+  if(elUsername && elPassword) {
+    elLoader.classList.toggle("none");
+    elButtonSpan.classList.toggle("none");
+    axios.post(api, dataUser).then(response => tokenGet(response)).catch(error => {
+      if(!toast) {
+        showToast("red", "Admin not found!");
+        toast = true;
+        setTimeout(() => {
+          toast = false;
+        }, 5000);
+      }
+      elLoader.classList.toggle("none");
+      elButtonSpan.classList.toggle("none");
+    });
+  }else {
+    if(!toast) {
+        showToast("red", "Fill Username and Password!");
+        toast = true;
+        setTimeout(() => {
+          toast = false;
+        }, 5000);
+      }
+  }
+  
+  function tokenGet(response) {
+    const jsonAdmin = JSON.stringify(dataUser);
+    localStorage.setItem("jsonAdmin", jsonAdmin);  
+    elLoader.classList.toggle("none");
+    elButtonSpan.classList.toggle("none");
+
+    setTimeout(() => {
+      elLogin.classList.add("none");
+      showToast("green", "Success!")
+    }, 1000);
+  }
+});
+
+function showToast(color, text) {
+    Toastify({
+      text: `${text}`, 
+      duration: 5000, // 5 soniya
+      gravity: "top", // Yuqori qism
+      position: "right", // O'ng tomon
+      close: false, 
+      stopOnFocus: true, 
+      style: {
+          background: "white",
+          color: "black", // Matn rangini qora qilamiz
+          borderRadius: "5px", // Yumaloq burchaklar
+          borderLeft: `5px solid ${color}`, // Chap tomonda ko'k rangli chiziq qo'shamiz (progress bar o'rniga)
+          // boxShadow: "0 3px 10px rgba(0,0,0,0.1)", // Engil soya
+          padding: "15px",
+          fontFamily: "Open Sans",
+          boxShadow: `2px 0px 10px -3px ${color}`,
+          
+      },
+      
+  }).showToast();
+}
 
