d984a8853fbac7d1d8b3a4334f30ae96c47ef126
usershokhrukh <asxurovsit@gmail.com>
2025-12-19 01:00:23 +0500

    delete method api 20%

--- utils/users.js
+++ utils/users.js
@@ -20,6 +20,8 @@ const elDashboardSearchForm = document.querySelector(".dashboard__search-form");
 let elUsersChangeSpanId = document.querySelector(".users__change-span-id");
 const elSearch = elDashboardSearchForm["search"];
 const elBody = document.querySelector("body");
+let elUsersNewForm = document.querySelector(".users__new-form");
+let elUsersAdd = document.querySelector(".users__add");
 var elUsersItems = document.querySelectorAll(".users__items");
 let indexItems = 0;
 var dataArray = [];
@@ -79,7 +81,6 @@ elUsersCard.innerHTML = `
             </div>
             <svg class="users__add" width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M13.0001 10.9999L22.0002 10.9997L22.0002 12.9997L13.0001 12.9999L13.0001 21.9998L11.0001 21.9998L11.0001 12.9999L2.00004 13.0001L2 11.0001L11.0001 10.9999L11 2.00025L13 2.00024L13.0001 10.9999Z"></path></svg>
     `;
-
 let elUsersChangeForm = document.querySelector(".users__change-form");
 let elUsersChangeExit = document.querySelector(".users__change-exit");
 form();
@@ -364,3 +365,17 @@ function checkInnerWidth() {
   }
 }
 window.addEventListener("resize", checkInnerWidth);
+
+
+
+form();
+function addF() {
+  elUsersNewForm.classList.toggle("none");
+  
+}
+function newFormF() {
+  elUsersNewForm.addEventListener("submit", (e)=> {
+    e.preventDefault();    
+  });
+}
+newFormF();
